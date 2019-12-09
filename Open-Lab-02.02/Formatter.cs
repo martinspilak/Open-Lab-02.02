88b138f16e53cd29f7334cd10201cf67bda715eb
martinspilak <martinecko24@gmail.com>
2019-12-09 09:47:48 +0100

    open-lab-02.02

--- Open-Lab-02.02/Formatter.cs
+++ Open-Lab-02.02/Formatter.cs
@@ -7,8 +7,14 @@ namespace Open_Lab_02._02
     {
         public void Format(double[] numbers)
         {
-            foreach (var number in numbers);
-            Console.Write(number.ToString("N2", CultureInfo.InvariantCulture) + " ");
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i].ToString("N2", CultureInfo.InvariantCulture));
+                
+                if (i < numbers.Length - 1)
+                    Console.Write(" ");
+            }
+            
         }
     }
 }
